4a7a1380f8e71b029f64f8acf539e3ae3bb49fdf
Sumeet Bansal <sumeetbansal@gmail.com>
2019-03-02 20:53:49 -0800

    added backend functionality for error data collection and storage

--- analytics/app.js
+++ analytics/app.js
@@ -24,6 +24,7 @@ app.use(session({
 }));
 
 app.use('/', require('./routes/index'));
+app.use('/api', require('./routes/api'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
