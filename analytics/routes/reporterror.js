3105532c123cc5dd400be961c0cf5a97909c9b2d
timferido <timferido@csu.fullerton.edu>
2019-03-03 16:55:40 -0800

    done with error report

--- analytics/routes/reporterror.js
+++ analytics/routes/reporterror.js
@@ -2,26 +2,22 @@ var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
 
-
-
 router.get('/', function(req, res, next) {
     const connection = mysql.createConnection({
         user    : 'root',
         password: 'space bar',
         database: 'collector'
     });
-
     connection.connect(function(err) {
-        if (err) throw err;
+        if (err) { console.log(err); }
         connection.query('SELECT * FROM error;', function (err, results, fields){
-            if (err) throw err;
-            console.log(results);
+            connection.end();
+            if (err) { console.log(err); }
+            res.render('reporterror', {data: results});
         });
     });
-    connection.end();
 
-    res.send(fields);
 });
 
 module.exports = router;
-  
+
