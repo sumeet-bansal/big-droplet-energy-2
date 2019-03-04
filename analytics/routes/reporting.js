0b42c1cd629d1b16e53aae563b73ad12f9b27a87
timferido <timferido@csu.fullerton.edu>
2019-03-03 19:53:00 -0800

    created new reporting path and refactored routes

---
+++ analytics/routes/reporting.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var mysql = require('mysql');
+var router = express.Router();
+
+const connection = mysql.createConnection({
+    user    : 'root',
+    password: 'space bar',
+    database: 'collector'
+});
+
+router.get('/error', function(req, res, next) {
+    connection.connect(function(err) {
+        if (err) { console.log(err); }
+        connection.query('SELECT * FROM error;', function (err, results, fields){
+            connection.end();
+            if (err) { console.log(err); }
+            res.render('reporterror', {data: results});
+        });
+    });
+});
+
+router.get('/random', function(req, res, next) {
+    connection.connect(function(err) {
+        if (err) { console.log(err); }
+        connection.query('SELECT * FROM random_load;', function (err, results, fields){
+            connection.end();
+            if (err) { console.log(err); }
+            res.render('reportrandom', {data: results});
+        });
+    });
+});
+
+router.get('/slow', function(req, res, next) {
+    connection.connect(function(err) {
+        if (err) { console.log(err); }
+        connection.query('SELECT * FROM slow_load;', function (err, results, fields){
+            connection.end();
+            if (err) { console.log(err); }
+            res.render('reportslow', {data: results});
+        });
+    });
+});
+
+router.get('/form', function(req, res, next) {
+    connection.connect(function(err) {
+        if (err) { console.log(err); }
+        connection.query('SELECT * FROM form_data;', function (err, results, fields){
+            connection.end();
+            if (err) { console.log(err); }
+            res.render('reportform', {data: results});
+        });
+    });
+});
+
+module.exports = router;
