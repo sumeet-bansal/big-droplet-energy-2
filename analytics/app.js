c941a5f175b5a874eb60c02bb200dc763e26026f
Sumeet Bansal <sumeetbansal@gmail.com>
2019-03-13 11:25:39 -0700

    removed old reporting routes

--- analytics/app.js
+++ analytics/app.js
@@ -26,10 +26,6 @@ app.use(session({
 app.use('/', require('./routes/index'));
 app.use('/api', require('./routes/api'));
 app.use('/reporting', require('./routes/reporting'));
-// app.use('/reporterror', require('./routes/reporterror'));
-// app.use('/reportrandom', require('./routes/reportrandom'));
-// app.use('/reportslow', require('./routes/reportslow'));
-// app.use('/reportform', require('./routes/reportform'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
