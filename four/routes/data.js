a7f6ec7f7387e52ed9de83f90cd97d2843f5bab7
Alberto Montilla <amontilla2008@gmail.com>
2019-03-14 18:23:08 -0300

    good reset

--- four/routes/data.js
+++ four/routes/data.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
 
+
 router.get('/technographics', function(req, res, next) {
     const connection = mysql.createConnection({
         user: 'root',
@@ -19,6 +20,74 @@ router.get('/technographics', function(req, res, next) {
     res.render('technographics', data);
 });
 
+router.get('/errors', function(req, res, next) {                           
+    const connection = mysql.createConnection({                                    
+        user: 'root',                                                              
+        password: 'space bar',                                                     
+        database: 'new_schema'                                                      
+    });                                                                            
+    var data;                                                                      
+    connection.connect(function(err) {                                             
+        if (err) { console.log(err); }                                             
+        connection.query('SELECT * FROM error;', [], function (err, results) {                         
+                if (err) { console.log(err); }                                     
+                data = results;         
+		
+		
+	 	// parse data                                                                  
+                var dr = {                                                          
+                        refErr: 0,                                                  
+                        typErr: 0,                                                  
+                        uriErr: 0,                                                  
+                        synErr: 0,                                                  
+                        rngErr: 0,                                                           
+                        evlErr: 0                                                   
+                } 
+
+		
+   	 	for (var i = 0; i < data.length; i++) {                                  
+   	 		var type = data[i].errorMessage;
+			var n = type.indexOf(':');	
+			type = type.substring(0, n != -1 ? n : type.length);
+
+			
+			if(type == "ReferenceError"){
+				dr.refErr = dr.refErr + 1;
+			}
+			else if( type == "Uncaught EvalError"){
+				dr.evlErr = dr.evlErr + 1;
+			}
+			else if( type == "Uncaught RangeError"){                    
+                                dr.rngErr = dr.rngErr + 1;                         
+                        }
+			else if( type == "Uncaught SyntaxError"){
+                                dr.synErr = dr.synErr + 1;
+                        }
+			else if( type == "Uncaught TypeError"){
+                                dr.typErr = dr.typErr + 1;
+                        }
+			else if( type == "Uncaught URIError"){
+                                dr.uriErr = dr.uriErr + 1;
+                        }
+
+
+    		} 
+
+		res.render('errors', {data: dr});
+
+
+
+		
+
+
+
+
+        });                                                                        
+    });                                                                                                            
+});
+
+
+/*
 router.get('/errors', function(req, res, next) {
     const connection = mysql.createConnection({
         user: 'root',
@@ -29,11 +98,17 @@ router.get('/errors', function(req, res, next) {
      
     connection.connect(function(err) {
         if (err) { console.log(err); }
-        connection.query('SELECT * FROM error;', [], function (err, results) {
-                if (err) { console.log(err); } 
-                data = results; 
-                return;
-        });
+        connection.query('SELECT img1 FROM random_load;', [], function (err, results) {
+       		if (err) {                                                 
+                                console.log(err);                                  
+                                return res.status(500).send({                      
+                                        message: 'Database read failed.'           
+                                });                                                
+                        }
+		data = results;
+		res.render('errors', data);
+			
+	}); 
     });
 
     // parse data
@@ -50,8 +125,13 @@ router.get('/errors', function(req, res, next) {
     //     console.log(type);
     // }
 
+    /*
     res.render('errors', data);
-});
+    */
+//});
+
+
+
 
 router.get('/performance', function(req, res, next) {
     res.render('performance');
