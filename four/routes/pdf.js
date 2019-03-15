714f388566c5e2ca81319d84d5e365868e45b53d
timferido <timferido@csu.fullerton.edu>
2019-03-15 12:30:41 -0700

    trying loging from puppeteer'

--- four/routes/pdf.js
+++ four/routes/pdf.js
@@ -3,16 +3,27 @@ var express = require('express');
 var router = express.Router();
   
 async function printPdf(p){
-  const browser = await puppeteer.launch({args: ['--enable-features=NetworkService', '--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors']});
-  const page = await browser.newPage();
-  var url = 'http://134.209.48.201:5002/data/' + p;
-  try {
+    const browser = await puppeteer.launch({args: ['--enable-features=NetworkService', '--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors']});
+    const page = await browser.newPage();
+    var url = 'http://134.209.48.201:5002';
+
+    // login
     await page.goto(url, {waitUntil: 'networkidle0'});
-  } catch (err) {
-      console.log(err);
-  }
-  await page.pdf({path: p + '.pdf', format: 'A4'});
-  await browser.close();
+    await page.type('#username', 'root');
+    await page.type('#password', 'space bar');
+    await page.click('#action-button');
+    await page.waitForNavigation();
+
+    // get cookies
+    const cookies = await page.cookies();
+
+    // set cookies to navigate as user
+    const page2 = await browser.newPage();
+    await page2.setCookie(...cookies);
+    await page2.goto(url+'/pdf/'+p, {waitUntil:'networkidle0'});
+
+    await page2.pdf({path: p + '.pdf', format: 'A4'});
+    await browser.close();
 };
 
 router.get('/:page', function(req, res, next){
