45c0cd2d5cee55b419c12b56499a2264a2287d82
Alberto Montilla <amontilla2008@gmail.com>
2019-03-15 14:14:55 -0300

    perf

--- four/routes/data.js
+++ four/routes/data.js
@@ -177,60 +177,11 @@ router.get('/errors', function(req, res, next) {
 			else if( type == "Uncaught URIError"){
                                 dr.uriErr = dr.uriErr + 1;
                         }
-
-
     		} 
-
 		res.render('errors', {data: dr});
         });                                                                        
     });                                                                                                            
 });
-
-
-/*
-router.get('/errors', function(req, res, next) {
-    const connection = mysql.createConnection({
-        user: 'root',
-        password: 'space bar',
-        database: 'collector'
-    });
-    var data;
-     
-    connection.connect(function(err) {
-        if (err) { console.log(err); }
-        connection.query('SELECT img1 FROM random_load;', [], function (err, results) {
-       		if (err) {                                                 
-                                console.log(err);                                  
-                                return res.status(500).send({                      
-                                        message: 'Database read failed.'           
-                                });                                                
-                        }
-		data = results;
-		res.render('errors', data);
-			
-	}); 
-    });
-
-    // parse data
-    // var dr = { 
-    //     refErr: {count:0}, 
-    //     typErr: {count:0}, 
-    //     uriErr: {count:0}, 
-    //     synErr: {count:0}, 
-    //     rngErr: {count:0}, 
-    //     evlErr: {count:0}
-    // }
-    // for (var i = 0; i < data.length; i++) {
-    //     var type = data[i].errorMessage;
-    //     console.log(type);
-    // }
-
-    /*
-    res.render('errors', data);
-    */
-//});
-
-
 
 
 router.get('/performance', function(req, res, next) {
